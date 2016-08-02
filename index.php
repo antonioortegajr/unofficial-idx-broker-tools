c3d9e057fc660ec94c2138766ffb825c82cc59ec
antonioortegajr <antonioortegajr@gmail.com>
2016-08-02 16:36:44 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+</head>
+<body>
+<h1>Unoffcial tools for IDX Broker</h1>
+<p>These are tools to be used with IDX Broker services. they UNoffcial and DO NOT contact IDX Broker support for ANY of these tools.</p>
+
+</body>
+</html>
