5849dd23406c9c8b75014f126349644b2e417499
antonioortegajr <antonioortegajr@gmail.com>
2016-08-02 21:26:44 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,16 @@
 <head>
 </head>
 <body>
-<h1>Unoffcial tools for IDX Broker</h1>
+<h1>Unofficial tools for IDX Broker</h1>
 <p>These are tools to be used with IDX Broker services. They are UNoffcial. DO NOT contact IDX Broker for support of ANY of these tools.</p>
-
+<h3><a href="site-analyzer">Site Analyzer</a></h3>
+<p>This tool does the following:
+<ul>
+<li>Mobile friendliness</li>
+<li>Google page Speed score</li>
+<li>Checks for iframes</li>
+<li>Creates link to check for number of indexed pages</li>
+</ul>
+</p>
 </body>
 </html>
