996cfe5d8e9f462f9c3464c8646a750bdd2d29f8
antonioortegajr <antonioortegajr@gmail.com>
2016-08-03 09:37:50 -0700

    added watch button
    added watch button

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
 <h1>Unofficial tools for IDX Broker</h1>
 <p>These are tools to be used with IDX Broker services. They are UNoffcial. DO NOT contact IDX Broker for support of ANY of these tools.</p>
 <p><!-- Place this tag where you want the button to render. -->
-<a class="github-button" href="https://github.com/antonioortegajr/unofficial-idx-broker-tools" data-icon="octicon-star" data-style="mega" data-count-href="/antonioortegajr/unofficial-idx-broker-tools/stargazers" data-count-api="/repos/antonioortegajr/unofficial-idx-broker-tools#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star antonioortegajr/unofficial-idx-broker-tools on GitHub">Star</a></p>
+<a class="github-button" href="https://github.com/antonioortegajr/unofficial-idx-broker-tools" data-icon="octicon-star" data-style="mega" data-count-href="/antonioortegajr/unofficial-idx-broker-tools/stargazers" data-count-api="/repos/antonioortegajr/unofficial-idx-broker-tools#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star antonioortegajr/unofficial-idx-broker-tools on GitHub">Star</a>
+<!-- Place this tag where you want the button to render. -->
+<a class="github-button" href="https://github.com/antonioortegajr/unofficial-idx-broker-tools" data-icon="octicon-eye" data-style="mega" data-count-href="/antonioortegajr/unofficial-idx-broker-tools/watchers" data-count-api="/repos/antonioortegajr/unofficial-idx-broker-tools#subscribers_count" data-count-aria-label="# watchers on GitHub" aria-label="Watch antonioortegajr/unofficial-idx-broker-tools on GitHub">Watch</a>
+</p>
 <h3><a href="site-analyzer">Site Analyzer</a></h3>
 <p>This tool does the following:
 <ul>
