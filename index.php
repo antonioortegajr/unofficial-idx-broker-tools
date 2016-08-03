81f0b2a27df14e8ef2359f6b34f35af49b244e78
antonioortegajr <antonioortegajr@gmail.com>
2016-08-02 21:14:21 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 </head>
 <body>
 <h1>Unoffcial tools for IDX Broker</h1>
-<p>These are tools to be used with IDX Broker services. they UNoffcial and DO NOT contact IDX Broker support for ANY of these tools.</p>
+<p>These are tools to be used with IDX Broker services. They are UNoffcial. DO NOT contact IDX Broker for support of ANY of these tools.</p>
 
 </body>
 </html>
