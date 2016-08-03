fb8bc93831b67322d3d4852696813b91b0420da2
antonioortegajr <antonioortegajr@gmail.com>
2016-08-02 21:38:24 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <html>
 <head>
+  <!-- Place this tag in your head or just before your close body tag. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
 </head>
 <body>
 <h1>Unofficial tools for IDX Broker</h1>
 <p>These are tools to be used with IDX Broker services. They are UNoffcial. DO NOT contact IDX Broker for support of ANY of these tools.</p>
+<p><!-- Place this tag where you want the button to render. -->
+<a class="github-button" href="https://github.com/antonioortegajr/unofficial-idx-broker-tools" data-icon="octicon-star" data-style="mega" data-count-href="/antonioortegajr/unofficial-idx-broker-tools/stargazers" data-count-api="/repos/antonioortegajr/unofficial-idx-broker-tools#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star antonioortegajr/unofficial-idx-broker-tools on GitHub">Star</a></p>
 <h3><a href="site-analyzer">Site Analyzer</a></h3>
 <p>This tool does the following:
 <ul>
