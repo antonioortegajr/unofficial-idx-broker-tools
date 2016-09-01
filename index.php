6dc2ca042cb3cfa886db2117a66fa63dc1a4c4c9
antonioortegajr <antonioortegajr@gmail.com>
2016-09-01 14:29:59 -0700

    fixed typos
    fixed typos

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body style="padding-left: 10%; padding-right: 10%;">
 <h1>Unofficial tools for use with IDX Broker</h1>
-<p>These are tools to be used with IDX Broker services. They are UNoffcial. DO NOT contact IDX Broker for support of ANY of these tools.</p>
+<p>These are tools to be used with IDX Broker services. They are UNofficial. DO NOT contact IDX Broker for support of ANY of these tools.</p>
 GitHub repo: <a href="https://github.com/antonioortegajr/unofficial-idx-broker-tools">https://github.com/antonioortegajr/unofficial-idx-broker-tools</a>
 <br>
 <p><!-- Place this tag where you want the button to render. -->
@@ -13,11 +13,11 @@ GitHub repo: <a href="https://github.com/antonioortegajr/unofficial-idx-broker-t
 <!-- Place this tag where you want the button to render. -->
 <a class="github-button" href="https://github.com/antonioortegajr/unofficial-idx-broker-tools" data-icon="octicon-eye" data-style="mega" data-count-href="/antonioortegajr/unofficial-idx-broker-tools/watchers" data-count-api="/repos/antonioortegajr/unofficial-idx-broker-tools#subscribers_count" data-count-aria-label="# watchers on GitHub" aria-label="Watch antonioortegajr/unofficial-idx-broker-tools on GitHub">Watch</a>
 </p>
-<p>About: These are tools were created with the intent of making tasks relating to IDX Broker easier. They are not part of any offcial IDX Broker product nor are they endorsed by IDX Broker. Each tool is a stand alone application. They are not dependant on each other in any way. See readme.md in the repo for more. Any issues/enhancements are only to be submitted via the GitHub repo.
+<p>About: These are tools were created with the intent of making tasks relating to IDX Broker easier. They are not part of any official IDX Broker product nor are they endorsed by IDX Broker. Each tool is a stand alone application. They are not dependant on each other in any way. See readme.md in the repo for more. Any issues/enhancements are only to be submitted via the GitHub repo.
 </p>
 <hr>
 <h3><a href="site-analyzer">Site Analyzer</a></h3>
-<p>When evaluating a web page there are many factors to take into consideration. How search engines may view being one of the biggest. Some automization can make this evealuation quicker and even self serve.</p>
+<p>When evaluating a web page there are many factors to take into consideration. How search engines may view being one of the biggest. Some automization can make this evaluation quicker and even self serve.</p>
 <p>This tool does the following:
 <ul>
 <li>Mobile friendliness</li>
@@ -36,7 +36,7 @@ GitHub repo: <a href="https://github.com/antonioortegajr/unofficial-idx-broker-t
   </ul>
   </p>
 <h3><a href="advanced-fields">Find Advacned Fields</a></h3>
-<p>Trying to find an advanced field in an MLS data set can be cumbersome. Instead this tool uses the API to bring all adnaced fields and creates a link to a list of field values. Each click on a field values link is an API call so be midful of rate API limits.
+<p>Trying to find an advanced field in an MLS data set can be cumbersome. Instead this tool uses the API to bring all advanced fields and creates a link to a list of field values. Each click on a field values link is an API call so be mindful of rate API limits.
   <p>
 <p>This tool does the following:
 <ul>
@@ -45,7 +45,7 @@ GitHub repo: <a href="https://github.com/antonioortegajr/unofficial-idx-broker-t
 </ul>
 </p>
 <h3><a href="mls-refresh">Check last MLS refresh</a></h3>
-<p>Chack the last refresh for approved MLS(s) on an account. Each check is one call for all approved MLSs and one for each indiviual MLS refresh time. So be middful of rate API limits.
+<p>Check the last refresh for approved MLS(s) on an account. Each check is one call for all approved MLSs and one for each individual MLS refresh time. So be mindful of rate API limits.
   <p>
 <p>This tool does the following:
 <ul>
