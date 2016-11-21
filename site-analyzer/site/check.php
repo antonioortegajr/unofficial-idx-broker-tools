2d72145930ec23d125bbc5dd4ce9523ab12f7fca
antonioortegajr <antonioortegajr@gmail.com>
2016-11-20 20:43:07 -0800

    abstracted CMS analyze
    abstracted CMS analyze

--- site-analyzer/site/check.php
+++ site-analyzer/site/check.php
@@ -6,63 +6,11 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_HEADER,true);
 $data = curl_exec($ch);
 
-
-//Look for CMS indentifiers in source
-echo '<h2>Is this site built on a CMS?</h2>';
-$wordpress_pattern = "/\bwp-(?:content|includes)\b/i";
-$wix_pattern = "/\bwix(?:.com|press)\b/i";
-$squarespace_pattern = "/\bsquarespace(?:.com|.com\/static)\b/i";
-$drupal_pattern = "/\bdrupal(?:.js|.settings)\b/i";
-$joomla_pattern = "/\bjui(?:\/js)\b/i";
-$weebly_pattern = "/\bassets-www1.weebly(?:.com)\b/i";
-$websitebox_pattern = "/\b\/wsbx(?:\/)\b/i";
-
-//WP test
-if (preg_match_all($wordpress_pattern, $data, $matches, PREG_SET_ORDER)) {
-    echo "Number of WordPress indicators found: ";
-    echo count($matches);
-}
-
-//WIX test
-elseif (preg_match_all($wix_pattern, $data, $matches)) {
-    echo "Number of WIX indicators found:";
-    echo count($matches);
-}
-
-//squarespace test
-elseif (preg_match_all($squarespace_pattern, $data, $matches)) {
-    echo "Number of squarespace indicators found:";
-    echo count($matches);
-}
-
-//drupal test
-elseif (preg_match_all($drupal_pattern, $data, $matches)) {
-    echo "Number of Drupal indicators found:";
-    echo count($matches);
-}
-
-//joomla test
-elseif (preg_match_all($joomla_pattern, $data, $matches)) {
-    echo "Number of Joomla indicators found:";
-    echo count($matches);
-}
-//weebly test
-elseif (preg_match_all($weebly_pattern, $data, $matches)) {
-    echo "A Weebly indicators found:";
-    echo count($matches);
-  }
-
-//websitebox test
-elseif (preg_match_all($weebly_pattern, $data, $matches)) {
-    echo "Number of WebsiteBox indicators found:.";
-    echo count($matches);
-}
-else{
-  echo 'Unable to find CMS idendifiers.';
-}
-echo '<hr>';
+//try and determin site type.
+include_once 'site-type/type.php';
 
 //checking for iframes
 echo '<h2>Check for iframes</h2>';
---
+++ site-analyzer/site/site-type/type.php
@@ -0,0 +1,61 @@
+<?php
+//Look for CMS indentifiers in source
+echo '<h2>Is this site built on a CMS?</h2>';
+$wordpress_pattern = "/\bwp-(?:content|includes)\b/i";
+$wix_pattern = "/\bwix(?:.com|press)\b/i";
+$squarespace_pattern = "/\bsquarespace(?:.com|.com\/static)\b/i";
+$drupal_pattern = "/\bdrupal(?:.js|.settings)\b/i";
+$joomla_pattern = "/\bjui(?:\/js)\b/i";
+$weebly_pattern = "/\bassets-www1.weebly(?:.com)\b/i";
+$websitebox_pattern = "/\b\/wsbx(?:\/)\b/i";
+
+//WP test
+if (preg_match_all($wordpress_pattern, $data, $matches, PREG_SET_ORDER)) {
+    echo "Number of WordPress indicators found: ";
+    echo count($matches);
+}
+
+//WIX test
+elseif (preg_match_all($wix_pattern, $data, $matches)) {
+    echo "Number of WIX indicators found:";
+    echo count($matches);
+}
+
+//squarespace test
+elseif (preg_match_all($squarespace_pattern, $data, $matches)) {
+    echo "Number of squarespace indicators found:";
+    echo count($matches);
+}
+
+//drupal test
+elseif (preg_match_all($drupal_pattern, $data, $matches)) {
+    echo "Number of Drupal indicators found:";
+    echo count($matches);
+}
+
+//joomla test
+elseif (preg_match_all($joomla_pattern, $data, $matches)) {
+    echo "Number of Joomla indicators found:";
+    echo count($matches);
+}
+//weebly test
+elseif (preg_match_all($weebly_pattern, $data, $matches)) {
+    echo "A Weebly indicators found:";
+    echo count($matches);
+  }
+
+//websitebox test
+elseif (preg_match_all($weebly_pattern, $data, $matches)) {
+    echo "Number of WebsiteBox indicators found:.";
+    echo count($matches);
+}
+else{
+  echo 'Unable to find CMS idendifiers.';
+}
+echo '<hr>';
+
+
+
+
+
+?>
