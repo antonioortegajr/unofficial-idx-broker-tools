48f57c3c89dad9a590b003b61a09e284ae4d25b6
antonioortegajr <antonioortegajr@gmail.com>
2016-11-28 19:31:30 -0800

    changed variable and added slash
    changed variable and added slash.

--- site-analyzer/sitemap/sitemap-check.php
+++ site-analyzer/sitemap/sitemap-check.php
@@ -1,7 +1,9 @@
 <?php
 
+$sitemap_url = $url.'sitemap.xml';
+
 //cURL the provided url
-$ch = curl_init($url.'sitemap.xml');
+$ch = curl_init($sitemap_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -13,12 +15,13 @@ echo '<h2>Site Map Check</h2>
 <p/>
 Returned Status of page: ';
 echo $map_code;
+echo ' Site Checked: ' . $sitemap_url;
 
 if($map_code == 200){
-  echo 'Site map found at '. $url .'/sitemap.xml';
+  echo 'Site map found at ' . $sitemap_url;
 }
 elseif($map_code < 300){
-  echo '<p>Sitemap not found at '. $url .'/sitemap.xml
+  echo '<p>Sitemap not found at ' . $sitemap_url . '
   </p>
   <p>IDX Broker offers a site maps for all clients. Both XML and HTML:</p>
   <p>
